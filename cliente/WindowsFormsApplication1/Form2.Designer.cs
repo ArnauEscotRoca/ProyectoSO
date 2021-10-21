8aa0073ef182f128f730d9adf0de8d179b084a4c
ArnauEscotRoca <91828899+ArnauEscotRoca@users.noreply.github.com>
2021-10-21 12:50:01 +0200

    Add files via upload

---
+++ cliente/WindowsFormsApplication1/Form2.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.grados = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.RadioButton();
+            this.F = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Desconectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // grados
+            // 
+            this.grados.Location = new System.Drawing.Point(183, 132);
+            this.grados.Name = "grados";
+            this.grados.Size = new System.Drawing.Size(100, 22);
+            this.grados.TabIndex = 2;
+            // 
+            // C
+            // 
+            this.C.AutoSize = true;
+            this.C.Location = new System.Drawing.Point(144, 192);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(94, 21);
+            this.C.TabIndex = 3;
+            this.C.TabStop = true;
+            this.C.Text = "De ºC a ºF";
+            this.C.UseVisualStyleBackColor = true;
+            // 
+            // F
+            // 
+            this.F.AutoSize = true;
+            this.F.Location = new System.Drawing.Point(144, 238);
+            this.F.Name = "F";
+            this.F.Size = new System.Drawing.Size(94, 21);
+            this.F.TabIndex = 4;
+            this.F.TabStop = true;
+            this.F.Text = "De ºF a ºC";
+            this.F.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Grados";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 45);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Enviar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.F);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.grados);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox grados;
+        private System.Windows.Forms.RadioButton C;
+        private System.Windows.Forms.RadioButton F;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
